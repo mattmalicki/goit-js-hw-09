dc166b2e4f985e70b92411cb381ed9fa0d7ed46a
mattmalicki <mateusz.malicki96@gmail.com>
2023-07-10 09:39:36 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let intervals = null;
 
+startBtn.addEventListener('click', () => {
+  intervals = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.setAttribute('disabled', '');
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervals);
+  startBtn.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
