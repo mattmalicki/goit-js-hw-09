6442dffca483d90dc718d1e1b77b70ea1243b949
mattmalicki <mateusz.malicki96@gmail.com>
2023-07-11 13:53:22 +0200

    checking

---
+++ src/js/02-timer.js
@@ -0,0 +1,36 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const inputDate = document.querySelector('input#datetime-picker');
+const startBttn = document.querySelector('button[data-start]');
+startBttn.setAttribute('disabled', '');
+
+flatpickr(inputDate, {
+  options,
+  enableTime: true,
+  closeOnSelect: false,
+  onClose: function (selectedDates) {
+    const currentDate = new Date();
+    selectedDates[0] < currentDate ? incorrectDate() : correctDate();
+  },
+});
+
+function incorrectDate() {
+  startBttn.setAttribute('disabled', '');
+  window.alert('Please choose date in the future!');
+}
+
+function correctDate() {
+  startBttn.removeAttribute('disabled');
+  console.log('Test');
+}
