b83332600793abc5b3eb5700dc03d5e12ab830be
mattmalicki <mateusz.malicki96@gmail.com>
2023-07-11 14:27:41 +0200

    smth

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,16 +10,17 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
-
 const inputDate = document.querySelector('input#datetime-picker');
 const startBttn = document.querySelector('button[data-start]');
 startBttn.setAttribute('disabled', '');
 
 flatpickr(inputDate, {
-  options,
+  ...options,
   onClose: function (selectedDates) {
     const currentDate = new Date();
-    selectedDates[0] < currentDate ? incorrectDate() : correctDate();
+    selectedDates[0] < currentDate
+      ? incorrectDate()
+      : correctDate(selectedDates[0], currentDate);
   },
 });
 
@@ -28,6 +29,18 @@ function incorrectDate() {
   window.alert('Please choose date in the future!');
 }
 
-function correctDate() {
+function correctDate(atr1, atr2) {
   startBttn.removeAttribute('disabled');
+  countdown(atr1, atr2);
+}
+
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+function countdown(selectedDate, currentDate) {
+  console.log(
+    `Thats a selected date - ${selectedDate} \n And thats a current date - ${currentDate}`
+  );
 }
