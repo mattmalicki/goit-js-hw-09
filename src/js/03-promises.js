cde2956cc28106b44ba7ea04a97824e0319d4e97
mattmalicki <mateusz.malicki96@gmail.com>
2023-07-13 19:29:57 +0200

    irono

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,21 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="delay"]');
+const amount = document.querySelector('input[name="amount"]');
+const startBttn = document.querySelector('button');
+const fulfilled = Notify.success(
+  `Fulfilled promise ${this.position} in ${this.delay}ms`
+);
+const rejected = Notify.failure(
+  `Rejected promise ${this.position} in ${this.delay}ms`
+);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    fulfilled;
   } else {
-    // Reject
+    rejected;
   }
 }
