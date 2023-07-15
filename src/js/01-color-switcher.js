1089f7723f52a2e1d9a3c5306cc5d44b7477b91a
mattmalicki <mateusz.malicki96@gmail.com>
2023-07-15 07:10:55 +0200

    something funny

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,9 +2,15 @@ const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 let intervals = null;
+const disable = name => {
+  name.disabled = true;
+};
+const enable = name => {
+  name.disabled = false;
+};
 
 const startClick = () => {
-  startBtn.setAttribute('disabled', '');
+  disable(startBtn);
   intervals = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
@@ -14,7 +20,7 @@ startBtn.addEventListener('click', startClick);
 
 stopBtn.addEventListener('click', () => {
   clearInterval(intervals);
-  startBtn.removeAttribute('disabled');
+  enable(startBtn);
 });
 
 function getRandomHexColor() {
